7597264270ef459aef571419c811033400c64889
halim <rimha93@gmail.com>
2022-04-05 03:45:10 +0900

    Fix: index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,13 +7,13 @@ import * as serviceWorker from "serviceWorker";
 
 //web3
 //App root의 provider로 제공하고 web3 객체를 인스턴스화 하는 getLibrary 함수 정의
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
+// import { Web3ReactProvider } from "@web3-react/core";
+// import { Web3Provider } from "@ethersproject/providers";
 
-function getLibrary(provider) {
-  const library = new Web3Provider(provider, "any");
-  return library;
-}
+// function getLibrary(provider) {
+//   const library = new Web3Provider(provider, "any");
+//   return library;
+// }
 
 ReactDOM.render(
   <React.StrictMode>
